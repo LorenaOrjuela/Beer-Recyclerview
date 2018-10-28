d21a151099a4d38db4249f731cfc828e3afe051d
LorenaOrjuela <42306567+LorenaOrjuela@users.noreply.github.com>
2018-10-27 20:33:39 -0500

    Agregue mis primeros cambios

---
+++ app/src/main/java/com/example/lorenaorjuela/recyclerview/beer_detail.java
@@ -0,0 +1,13 @@
+package com.example.lorenaorjuela.recyclerview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class beer_detail extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beer_detail);
+    }
+}
